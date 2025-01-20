634338312571e7c943b149d308b6d9ee00f80217
Stephen-Kisangi <159438058+Stephen-Kisangi@users.noreply.github.com>
2025-01-20 20:00:03 +0300

    Update cardGame.js

--- cardGame.js
+++ cardGame.js
@@ -22,6 +22,12 @@ function drawCard() {
 
     // Optionally display the card in the console for debugging
     console.log(`Drawn card: ${drawnCard.suit} ${drawnCard.value}`);
+
+     gtag('event', 'draw_card', {
+        'event_category': 'Game Interaction',
+        'event_label': `${drawnCard.value} of ${drawnCard.suit}`,
+        'value': 1  // Optional value, can represent the action's significance or the number of draws
+    });
 }
 
 // Function to check the user's guess
@@ -45,4 +51,49 @@ function checkGuess() {
         message.innerText = '❌ Better luck next time! Keep trying!';
         message.style.color = 'darkred';
     }
+    // Track Guess Check Event in Google Analytics
+    gtag('event', 'check_guess', {
+        'event_category': 'Game Interaction',
+        'event_label': `${guessedValue} of ${guessedSuit}`,
+        'guessed_value': guessedValue,
+        'guessed_suit': guessedSuit
+    });
+
+    if (guessedSuit === drawnCard.suit && guessedValue === drawnCard.value) {
+        document.querySelector('h1').innerText = 'You got it!';
+        // Track Correct Guess Event
+        gtag('event', 'correct_guess', {
+            'event_category': 'Game Interaction',
+            'event_label': 'Correct Guess'
+        });
+    } else {
+        document.querySelector('h1').innerText = 'Better Luck Next Time!';
+        // Track Incorrect Guess Event
+        gtag('event', 'incorrect_guess', {
+            'event_category': 'Game Interaction',
+            'event_label': 'Incorrect Guess'
+        });
+    }
 }
+
+
+// Track page load time
+window.onload = function () {
+    const pageLoadTime = performance.timing.loadEventEnd - performance.timing.navigationStart;
+    gtag('event', 'timing_complete', {
+        'name': 'page_load',
+        'value': pageLoadTime,
+        'event_category': 'Performance',
+        'event_label': 'Page Load Time'
+    });
+};
+
+
+// Track Button Click Event
+document.querySelector('button').addEventListener('click', function() {
+    gtag('event', 'button_click', {
+        'event_category': 'UI Interaction',
+        'event_label': 'Draw Card Button Click'
+    });
+});
+
